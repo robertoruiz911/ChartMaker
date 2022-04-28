90e4b9e7748f0b889bb1b405340036101060b6d5
Andres Soto <andresegidiosoto@gmail.com>
2022-04-27 19:17:26 -0500

    Update app.R

--- app.R
+++ app.R
@@ -2,7 +2,8 @@
 # By: AESoto
 # 20 April 2022
 
-chartLibraries <- c('quantmod', 'tidyverse', 'highcharter', 'tidyquant', 'timetk', 'shinyWidgets', 'shiny' )
+chartLibraries <- c('quantmod', 'tidyverse', 'highcharter',
+                    'tidyquant', 'timetk', 'shinyWidgets', 'shiny' )
 lapply(chartLibraries, require, character.only=TRUE)
 
 ############################
@@ -19,67 +20,77 @@ lapply(chartLibraries, require, character.only=TRUE)
 
 # Define UI for application that draws charts
 ui <- fluidPage(
-
-  # Application title
-  titlePanel("Chart Maker"),
-
-  # Sidebar with a slider input for number of bins 
-  fluidRow(
-    column(2,
-      awesomeRadio(
-        inputId = "Id003",
-        label = "Metric",
-        choices = c("10Yr Yield", "Something1", "Something2"),
-        selected = "10Yr Yield",
-        status = "warning"
+  
+  sidebarLayout(
+    # Application title
+    titlePanel("Chart Maker"),
+    
+    # Sidebar with a slider input for number of bins 
+    fluidRow(
+      column(2,
+             awesomeRadio(
+               inputId = "Id003",
+               label = "Metric",
+               choices = c("10Yr Yield", "Something1", "Something2"),
+               selected = "10Yr Yield",
+               status = "warning"
+             )
       )
+    ),
+    
+    fluidRow(
+      column(2,
+             dateInput("start",
+                       "Start Date",
+                       "2013-01-01",
+                       format = "yyyy-mm-dd")
+      )
+    ),
+    
+    fluidRow(
+      column(2,
+             dateInput("end",
+                       "End Date",
+                       "2022-04-18",
+                       format = "yyyy-mm-dd")
+      )
+    ),
+    
+    fluidRow(
+      column(2,  
+        actionButton("go", "Submit"),
+      )
+    ),
+    # Generate chart
+    mainPanel(
+      plotOutput("distPlot")
     )
-  ),
-  
-  fluidRow(
-    column(2,
-      dateInput("start",
-                "Start Date",
-                "2013-01-01",
-                format = "yyyy-mm-dd")
-    )
-  ),
-  
-  fluidRow(
-    column(2,
-       dateInput("end",
-                 "End Date",
-                 "2022-04-18",
-                 format = "yyyy-mm-dd")
-    )
-  ),
-  
-  actionButton("go", "Submit"),
-  
-  # Generate chart
-  mainPanel(
-     plotOutput("distPlot")
   )
 )
 
 
 # Define server logic required to draw chart
 server <- function(input, output) {
+  
+  observe({
+    print(input$end)
+  })
+  
   timeSeries <- eventReactive(input$go, {
     # will apply switch statement to accomodate other chart subjects
     getSymbols('^TNX', from = as.Date(input$start), to=as.Date(input$end))
     tenYr <- na.omit(TNX)
     tenYr$TNX.Volume <- NULL
-    )
-    timeSeries <- tenYr
+  
+  timeSeries <- tenYr
   })  # closes timeSeries function....
 
-  output$distPlot <- renderPlot({
-    # this applies the quantmod charting...
-    chartSeries(timeSeries,
-                type="line",
-                theme=chartTheme('white'))
-  }) # closes renderPlot function
+output$distPlot <- renderPlot({
+  # this applies the quantmod charting...
+  chartSeries(timeSeries,
+              type="line",
+              theme=chartTheme('white'))
+}) # closes renderPlot function
 }
 
 # Run the application 
